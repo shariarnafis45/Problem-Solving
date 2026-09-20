3afa59cd19f2347542f36e7d8f478b3ddeab0dc1
shariarnafis45 <nafisshahworkmail@gmail.com>
2026-09-20 11:26:16 +0600

    // Problem 18: FizzBuzz

--- Day-4/fizzBuzz.js
+++ Day-4/fizzBuzz.js
@@ -1,1 +1,19 @@
-// Problem 18: FizzBuzz 
+// Problem 18: FizzBuzz
+
+const fizzBuzz = (value) => {
+  const result = [];
+  for (let i = 1; i < value + 1; i += 1) {
+    if (i % 3 === 0  && i % 5 === 0) {
+      result.push("fizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("buzz");
+    } else if (i % 3 === 0 ) {
+      result.push("fizz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+console.log(fizzBuzz(15));
